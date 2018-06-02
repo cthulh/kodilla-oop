ccd216c239112d230f870f4099a1709a403d85bf
Mariusz Borowiak <borowiakm85@gmail.com>
2018-06-02 23:29:37 +0100

    Add buttons creation func

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function(){
+  // Slide prototype
   var slide = {
     whatId: function(){
       return "The slide id is " + this.id + ".";
@@ -15,17 +16,31 @@
       return "Coordinates of the location are: Latitude " + this.coords.lat + ", Longitude " + this.coords.lng;
     }
   }
+  // Button prototype
+  var button = {
+    text: "Hello!",
+    create: function(){
+      var self = this;
+      this.element = document.createElement('button');
+    	this.element.innerText = this.text;
+      this.element.className += "simpleBtn";
+    	this.element.addEventListener('click', function() {alert(self.text);});
+      document.body.appendChild(this.element);
+    }
+  }
 
   params.data.forEach(function(element, index){
-    params.objects[index] = Object.create(slide);
-    params.objects[index].id = element.id;
-    params.objects[index].image = element.image;
-    params.objects[index].description = element.description;
-    params.objects[index].title = element.title;
-    params.objects[index].coords = element.coords;
-    console.log("Object " + params.objects[index] + " was created.");
-    console.log("Prototype: " + params.objects[index].__proto__);
+    // ---------------------------------- slides
+    params.objects.slides[index] = Object.create(slide);
+    params.objects.slides[index].id = element.id;
+    params.objects.slides[index].image = element.image;
+    params.objects.slides[index].description = element.description;
+    params.objects.slides[index].title = element.title;
+    params.objects.slides[index].coords = element.coords;
+    // ---------------------------------- buttons
+    params.objects.buttons[index] = Object.create(button);
+    params.objects.buttons[index].text = element.title;
   });
 
-
+  params.objects.buttons.forEach(btn => btn.create());
 }());
